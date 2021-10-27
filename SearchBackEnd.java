d71b9585d5881a9dd42a0f50b868bd722a1c7eaf
Julia Oghigian <oghigian@wisc.edu>
2021-10-27 02:57:04 +0000

    Updated SearchBackEnd.java with comments

--- SearchBackEnd.java
+++ SearchBackEnd.java
@@ -1,3 +1,16 @@
+// --== CS400 Project Two File Header ==--
+// Name: Julia Oghigian
+// Email: oghigian@wisc.edu 
+// Team: Red
+// Group: BA 
+// TA: Cameron Ruggles
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
+/**
+ * This interface generates methods need to create a RedBlack Tree in  SearchBackEnd.
+ * 
+ * @author Julia Oghigian
+ */
 public interface SearchBackEndInterface<T extends Comparable<T>> extends Iterable<T> {
     // Note that the provided iterators step through the data within this
     // collection in sorted order, as defined by their compareTo() method.
@@ -11,36 +24,68 @@ public interface SearchBackEndInterface<T extends Comparable<T>> extends Iterabl
     public boolean isEmpty();
 
 }
+/**
+ * This class generates a RedBlack Tree for movies  while implementing SearchFrontEndInterface.
+ *
+ * @author Julia Oghigian
+ */
 public class SearchBackEnd implements SearchBackEndInterface {
-
+	     /**
+	     * Adds the given movie to the tree.
+	     *
+	     * @param movie The movie to be added to the tree.
+	     */
     @Override
     public boolean insert(MovieDataInterface movie) {
         // TODO Auto-generated method stub
 
     }
-
+             /**
+	     * Determines if a movie has been added to the tree.
+	     *
+	     * @param movie  The movie being searched for.
+	     * @return true if movie is in tree else return false.
+	     */
     @Override
     public boolean containsMovie(MovieDataInterface movie) {
         // TODO Auto-generated method stub
         return false;
     }
-
+            /**
+             * Finds the titles at the specific node using movieRank as a key.
+             *
+             * @param movieRank key
+             * @returns List contain movie titles
+             */
     @Override
     public List<String> findTitles(String movieRank) {
         // TODO Auto-generated method stub
         return null;
     }
-
+  	    /**
+	     * Finds the director at the specific node using movieRank as a key.
+	     *
+	     * @param movieRank key
+	     * @returns List contain director names
+	     */
     @Override
     public List<String> findDirector(String movieRank) {
         // TODO Auto-generated method stub
         return null;
     }
-
+	     /**
+	     * Finds the size of the tree.
+	     *
+	     * @return size
+	     */
     @Override
     public int size(){
 	return 0;
     }
+    	     /**
+	     * Determines if RedBlack tree is empty or not.	     *
+	     * @return true if tree is empty else returns false.
+	     */
     @Override
     public boolean isEmpty(){
 	return false;
