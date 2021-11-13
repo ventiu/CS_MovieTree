bc9643b133488df9b77e026a5523b1ca01d82892
Emma <kashton@wisc.edu>
2021-11-13 19:36:59 +0000

    fixed typos in MovieSearchApp

--- MovieSearchApp.java
+++ MovieSearchApp.java
@@ -4,9 +4,9 @@ public class MovieSearchApp {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Movie Search Trivia");
-        List<MovieDataInterface> moviess = new MovieLoader().loadAllFilesInDirectory("./data/");
-        SearchBackEndInterface engine = new SearchBackEnd();
-        for(MovieDataInterface movie : movies) engine.addSong(movie);
+        List<MovieDataInterface> movies = new MovieLoader().loadAllFilesInDirectory("./data/");
+        SearchBackEnd engine = new SearchBackEnd();
+        for(MovieDataInterface movie : movies) engine.insert(movie);
         SearchFrontEndInterface ui = new SearchFrontEnd();
         ui.run(engine);
     }
